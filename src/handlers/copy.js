ff587956785d87459254bdb28cdebb4ef2910b38
Nicolas Guyon <nicolas.guyon@telecomnancy.net>
2022-10-19 10:51:54 +0200

    reaplaced touch_delayed by simple console.log notification

--- src/handlers/copy.js
+++ src/handlers/copy.js
@@ -2,56 +2,12 @@
 
 var path = require('path');
 var fs = require('fs-extra');
-var process = require('process');
 
-function gettime() {
-	var hrTime = process.hrtime();
-	return 	hrTime[0] * 1000000 + hrTime[1] / 1000;
-}
-
-function touch (filename) {
-
-	console.log('[touch]', filename);
-	console.log(gettime());
-
-	const fs = require('fs');
-	const time = new Date();
-
-	try {
-	  fs.utimesSync(filename, time, time);
-	} catch (err) {
-	  fs.closeSync(fs.openSync(filename, 'w'));
-	}
-
-}
-
-var calls = {};
-
-function touch_delayed(src, filename) {
-
-	const time = gettime();
-	var timeout = 0;
-
-	console.log('time='+time+' calls[src]='+calls[src]);
-	console.log('diff='+(time - calls[src])); 
-
-	const minTimeout = 15;
-
-	if(src in calls) {
-		if(time - calls[src] < minTimeout * 1000) {
-			timeout = minTimeout;
-		}
-	}
-
-	calls[src] = time+timeout*1000;
-
-	// const timeout = Math.random()*50;
-	console.log('timeout ' + timeout);
-	setTimeout(() => touch(filename), timeout);
+function touch(src, dest) {
+    console.log('[touched]', src, '->', dest);
 }
 
 function link (src, dest) {
-
 	console.log('[copy]', src, '->', dest);
 
 	const parent = path.dirname(dest);
@@ -69,7 +25,6 @@ function link (src, dest) {
 		} catch(e) {
 			console.log(e);
 		}
-
 	});
 }
 
@@ -84,8 +39,8 @@ module.exports = function (config) {
 				if (f.exists) {
 					if(!fs.existsSync(dest)) {
 						link(src, dest);	
-					} else {
-						touch_delayed(src, dest);
+                    } else {
+						 touch(src, dest);
 					}
 				} else {
 					console.log('[delete]', dest);
@@ -93,5 +48,6 @@ module.exports = function (config) {
 				}
 			}
 		}
+        console.log("");
 	}
 }
